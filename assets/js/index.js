af173eb5941a105e1c599fdba0fca6e304fec61d
Mario Tonelli <mariosergio_tonelli@hotmail.com>
2018-09-28 01:23:06 -0300

    Criação do Banner Mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -61,3 +61,67 @@ $(document).on('ready', function() {
 	});
 
 });
+
+$(document).on('ready', function() {
+
+	$(".bannerMobile").slick({
+
+		dots: false,
+
+		infinite: true,
+
+		slidesToShow: 1,
+
+		slidesToScroll: 1,
+
+		autoplay: true,
+
+		autoplaySpeed: 2000,
+
+		prevArrow: $('.prev'),
+
+		nextArrow: $('.next'),
+
+		responsive: [
+
+		{
+
+			breakpoint: 600,
+
+			settings: {
+
+				slidesToShow: 1,
+
+				slidesToScroll: 1,
+
+				prevArrow: null,
+
+				nextArrow: null,
+
+			}
+
+		},
+
+		{
+
+			breakpoint: 480,
+
+			settings: {
+
+				slidesToShow: 1,
+
+				slidesToScroll: 1,
+
+				prevArrow: null,
+
+				nextArrow: null,
+
+			}
+
+		}
+
+		]
+
+	});
+
+});
